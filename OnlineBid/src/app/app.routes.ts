ca3c525e31db9b841279fd9626c89841ff7beb4b
shkelzenii <shkelzenkrasniqi01@gmail.com>
2024-10-05 18:28:30 +0200

    created footer , and redesigned navbar

--- OnlineBid/src/app/app.routes.ts
+++ OnlineBid/src/app/app.routes.ts
@@ -5,5 +5,5 @@ import { RegisterComponent } from './components/register/register.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  //{ path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
